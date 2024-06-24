441f0a9711fbf9d1ad5dd171b7212c49de50940f
İrem Özdemir <irem.ozdemir@etu.edu.tr>
2024-06-24 18:33:24 +0300

    basic setup

---
+++ code/main.py
@@ -0,0 +1,36 @@
+#display surface:
+#the canvas that everything will be drawn on
+#you can only have one at a time
+
+#event loop
+#check events
+#this also include x to close the game
+
+#print(pygame.ver)
+
+#pygame.init() tam tersi pygame.quit()
+
+#pygame.display.update()
+#tüm ekranı günceller
+#pygame.display.flip()
+#ekranın bir kısmını günceller
+
+import pygame 
+
+#general setup
+pygame.init()
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+running = True
+
+while running:
+    #event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    #draw the game
+    pygame.display.update()
+    
+pygame.quit()
